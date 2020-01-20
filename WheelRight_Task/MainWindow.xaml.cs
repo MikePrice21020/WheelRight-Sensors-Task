dc37be950da4a0ec3a5fd19ab28a3a9476077759
Michael <Michael@DESKTOP-UHQO68S>
2020-01-20 15:12:34 +0100

    Add project files.

---
+++ WheelRight_Task/MainWindow.xaml.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WheelRight_Task
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // A little simple click for generating a ID
+        // Is used to nicely display information on  the data grid via WPF
+        private static int id = 1;
+        static int GenerateId()
+        {
+            return id++;
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog 
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+
+
+            // Set filter for file
+            dlg.DefaultExt = ".txt";
+            dlg.Filter = "Text Files (*.txt)|*.txt";
+
+
+            Nullable<bool> result = dlg.ShowDialog();
+
+
+            if (result == true)
+            {
+                // Open document 
+                string filename = dlg.FileName;
+                searchBox.Text = filename;
+
+
+                // Used to debug the sensor sensitivity
+                int sensor_sensitivity;
+                try
+                {
+                    // Range of 1 - 6
+                    if (Int32.Parse(sensSensivity.Text) > 0 && Int32.Parse(sensSensivity.Text) <= 6)
+                    {
+                        sensor_sensitivity = Int32.Parse(sensSensivity.Text);
+                    }
+                    else
+                    {
+                        // Invalid
+                        MessageBox.Show("Sensitivity range is 1 - 6, sensitivity has been set to 2 (default)", "Invalid Input", MessageBoxButton.OK, MessageBoxImage.Exclamation, MessageBoxResult.OK);
+                        sensor_sensitivity = 2;
+                        sensSensivity.Text = "2";
+                    }
+                }
+                catch (System.FormatException)
+                {
+                    // Error
+                    MessageBox.Show("Single number only, sensitivity has been set to 2 (default)", "Invalid Input", MessageBoxButton.OK, MessageBoxImage.Exclamation, MessageBoxResult.OK);
+                    sensor_sensitivity = 2;
+                    sensSensivity.Text = "2";
+                }
+
+                // Create object with all data given in parameters
+                Sensor_Data dataSet = new Sensor_Data(Read_textData(filename), sensor_sensitivity);
+
+                // Load in Ambient temperatures
+                leftSensorAmbient.Text = dataSet.Left_Sensor_Ambient.ToString() + "°C";
+                rightSensorAmbient.Text = dataSet.Right_Sensor_Ambient.ToString() + "°C";
+
+                // Clear datagrid if new text file is loaded
+                leftSensorGrid.Items.Clear();
+                rightSensorGrid.Items.Clear();
+
+                id = 1;
+                // Load in type data, highest and average temperatures (Left sensor)
+                for (int i = 0; i < dataSet.CarData.Left_Wheel_Logs.Count; i++)
+                {
+                    Wheel_Data current = dataSet.CarData.Left_Wheel_Logs[i];
+                    current.Id = GenerateId();
+                    leftSensorGrid.Items.Add(current);
+                }
+                id = 1;
+                // Load in type data, highest and average temperatures (right sensor)
+                for (int i = 0; i < dataSet.CarData.Right_Wheel_Logs.Count; i++)
+                {
+                    Wheel_Data current = dataSet.CarData.Right_Wheel_Logs[i];
+                    current.Id = GenerateId();
+                    rightSensorGrid.Items.Add(current);
+                }
+            }
+        }
+        /// <summary>
+        /// Reads a text file and returns an array of strings
+        /// </summary>
+        /// <param name="file_directory"></param>
+        /// <returns></returns>
+        string[] Read_textData(string file_directory)
+        {
+            List<string> all_lines = new List<string>();
+            bool skip_first_line = true;
+
+            const Int32 BufferSize = 512;
+            using (var fileStream = File.OpenRead(file_directory))
+            using (var streamReader = new StreamReader(fileStream, Encoding.UTF8, true, BufferSize))
+            {
+                string line;
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    // Uses a simple boolean to skip the first line
+                    // This is assuming that the format is consistent
+                    if (skip_first_line == false)
+                        all_lines.Add(line);
+                    else
+                        skip_first_line = false;
+                }
+            }
+            return all_lines.ToArray();
+        }
+    }
+}
---
+++ WheelRight_Task/Sensor.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WheelRight_Task
+{
+    /// <summary>
+    /// Contain sets of tyre data
+    /// </summary>
+    public class Car_Data
+    {
+        public List<Wheel_Data> Right_Wheel_Logs { get; set; } = new List<Wheel_Data>();
+        public List<Wheel_Data> Left_Wheel_Logs { get; set; } = new List<Wheel_Data>();
+
+    }
+    /// <summary>
+    /// All data on individual tyres
+    /// </summary>
+    public class Wheel_Data
+    {
+        // This is used in datagrid(WPF) to display each record in the correct order
+        public int Id { get; set; }
+
+        // Contains all values recorded of the tyre temperature
+        public int[] Tyre_Detect { get; set; }
+
+        public int Sample_Size { get; set; }
+
+        public string Average_Temp { get; set; }
+        public string Highest_Temp { get; set; }
+
+        public Wheel_Data(int[] wheel_detection, int highest_temp)
+        {
+            Tyre_Detect = wheel_detection;
+            Highest_Temp = highest_temp.ToString() + "°C";
+            Sample_Size = wheel_detection.Count();
+
+            // Calculate average temperature
+            int sum = 0;
+            foreach (int x in Tyre_Detect)
+            {
+                sum += x;
+            }
+            Average_Temp = (sum / Tyre_Detect.Length).ToString() + "°C";
+        }
+
+    }
+    /// <summary>
+    /// Contains logic for sensor data
+    /// </summary>
+    public class Sensor_Data
+    {
+        // Parameters
+        public int Sensor_Sensitivity { get; set; }
+        public int Spike_Tolerance = 3;
+
+        // Left Sensor data
+        public int[] Left_Sensor_Data { get; set; }
+        public int Left_Sensor_Ambient { get; set; }
+
+        // Right Sensor data
+        public int[] Right_Sensor_Data { get; set; }
+        public int Right_Sensor_Ambient { get; set; }
+
+        // Contains left and right tyre readings
+        public Car_Data CarData { get; set; } = new Car_Data();
+
+        public Sensor_Data(string[] raw_list, int sensor_sensitivity)
+        {
+            Sensor_Sensitivity = sensor_sensitivity;
+
+            // Split on these chars
+            char[] delimiterChars = { ' ', '\t' };
+
+            // Set array size
+            Right_Sensor_Data = new int[raw_list.Length];
+            Left_Sensor_Data = new int[raw_list.Length];
+
+            // With the raw list given, find right and left sensor numbers
+            for (int i = 0; i < raw_list.Length; i++)
+            {
+                string[] split = raw_list[i].Split(delimiterChars);
+                try
+                {
+                    // I used 2 single arrays instead of a 2D array for the sake of clarity in further calucations
+                    Left_Sensor_Data[i] = Int32.Parse(split[0].Trim());
+                    Right_Sensor_Data[i] = Int32.Parse(split[1].Trim());
+                }
+                catch (System.FormatException)
+                {
+                    // Invalid text file was selected
+                    MessageBox.Show("Invalid Text file", "Error", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK);
+                    return;
+                }
+            }
+            // Used void methods for the sake of clarity and better work flow
+            Calculate_Ambient_Temperature();
+            Tyre_Detection();
+        }
+        /// <summary>
+        /// Searches the data for tyre readings and also removes spikes in data
+        /// </summary>
+        void Tyre_Detection()
+        {
+            int highest_temp = -999;
+            List<int> wheel_detection = new List<int>();
+
+            // Right Wheel
+            for (int i = 0; i < Right_Sensor_Data.Length; i++)
+            {
+                // Knowing the ambient temperature, and setting up a sensitivity value
+                // loop through data to find (relatively) dramatic increases in temperature
+                if (Right_Sensor_Data[i] > (Right_Sensor_Ambient + Sensor_Sensitivity))
+                {
+                    // A value that supasses (ambient temperature + sensitivity), Now each Subsequent value is recorded
+                    // Until it drops down below the threshold, which implies the tyre has passed
+                    for (int x = i; x < Right_Sensor_Data.Length; x++)
+                    {
+                        // This is here to see if it drops below the threshold
+                        if (Right_Sensor_Data[x] > (Right_Sensor_Ambient + Sensor_Sensitivity))
+                        {
+                            // Add to collection of tyre readings
+                            wheel_detection.Add(Right_Sensor_Data[x]);
+
+                            // Since we are looping through the tyre readings, we might aswell look for the highest value
+                            if (Right_Sensor_Data[x] > highest_temp)
+                            {
+                                highest_temp = Right_Sensor_Data[x];
+                            }
+                        }
+                        else
+                        {
+                            // Stopped recording, this statement checks if it could of been a spike/glitch
+                            // If the sample size was too small it wont create a wheel data 
+                            // This could be improved/more advanced
+                            if (wheel_detection.Count > Spike_Tolerance)
+                            {
+                                Wheel_Data rightwheel = new Wheel_Data(wheel_detection.ToArray(), highest_temp);
+                                CarData.Right_Wheel_Logs.Add(rightwheel);
+                            }
+
+                            // Step out and continue where we left off
+                            i = x;
+                            // Reset
+                            highest_temp = -999;
+                            wheel_detection.Clear();
+                            break;
+                        }
+                    }
+                }
+
+
+            }
+
+            // Left Wheel
+            for (int i = 0; i < Left_Sensor_Data.Length; i++)
+            {
+                if (Left_Sensor_Data[i] > (Left_Sensor_Ambient + Sensor_Sensitivity))
+                {
+                    for (int x = i; x < Left_Sensor_Data.Length; x++)
+                    {
+                        if (Left_Sensor_Data[x] > (Left_Sensor_Ambient + Sensor_Sensitivity))
+                        {
+                            wheel_detection.Add(Left_Sensor_Data[x]);
+
+                            if (Left_Sensor_Data[x] > highest_temp)
+                            {
+                                highest_temp = Left_Sensor_Data[x];
+                            }
+                        }
+                        else
+                        {
+                            if (wheel_detection.Count > Spike_Tolerance)
+                            {
+                                Wheel_Data leftwheel = new Wheel_Data(wheel_detection.ToArray(), highest_temp);
+                                CarData.Left_Wheel_Logs.Add(leftwheel);
+                            }
+
+                            i = x;
+                            highest_temp = -999;
+                            wheel_detection.Clear();
+                            break;
+                        }
+                    }
+                }
+
+
+            }
+        }
+        /// <summary>
+        /// Finds the Ambient Temperature
+        /// </summary>
+        void Calculate_Ambient_Temperature()
+        {
+            // Right sensor grouped data to show frequency of numbers
+            var right_Sensor_MetaData = from numbers in Right_Sensor_Data
+                                        group numbers by numbers into grouped
+                                        select new { Number = grouped.Key, Freq = grouped.Count() };
+
+            // Left sensor grouped data to show frequency of numbers
+            var left_Sensor_MetaData = from numbers in Left_Sensor_Data
+                                       group numbers by numbers into grouped
+                                       select new { Number = grouped.Key, Freq = grouped.Count() };
+
+            int frequency = 0;
+            int index_of_highest = 0;
+            int current_frequency = 0;
+
+            for (int q = 0; q < right_Sensor_MetaData.Count(); q++)
+            {
+                // Loop through frequency values on the right sensor to find the number with the highest frequency
+                current_frequency = Int32.Parse(right_Sensor_MetaData.ElementAt(q).Freq.ToString());
+                if (frequency < current_frequency)
+                {
+                    frequency = current_frequency;
+                    index_of_highest = q;
+                }
+            }
+
+            // Set ambient temperature of right sensor
+            Right_Sensor_Ambient = Int32.Parse(right_Sensor_MetaData.ElementAt(index_of_highest).Number.ToString());
+
+
+            frequency = 0;
+            index_of_highest = 0;
+            current_frequency = 0;
+
+            for (int q = 0; q < left_Sensor_MetaData.Count(); q++)
+            {
+                // Loop through frequency values on the left sensor to find the number with the highest frequency
+                current_frequency = Int32.Parse(left_Sensor_MetaData.ElementAt(q).Freq.ToString());
+                if (frequency < current_frequency)
+                {
+                    frequency = current_frequency;
+                    index_of_highest = q;
+                }
+            }
+
+            // Set ambient temperature of left sensor
+            Left_Sensor_Ambient = Int32.Parse(left_Sensor_MetaData.ElementAt(index_of_highest).Number.ToString());
+        }
+
+    }
+}
